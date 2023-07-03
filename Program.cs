6ed813621160a52db97fefdbfe04f7597ad09274
otherfutures <109718981+otherfutures@users.noreply.github.com>
2023-07-04 04:34:09 +0900

    Added custom resolution option

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace ImageFileNamer
         private TextBox inputLimitBox;
         private CheckBox closeCheckBox;
         private CheckBox confirmationMsg;
+        private TextBox sizeBox;
         private Button generateButton;
 
         [STAThread]
@@ -39,6 +40,7 @@ namespace ImageFileNamer
             generateButton = new Button();
             closeCheckBox = new CheckBox();
             confirmationMsg = new CheckBox();
+            sizeBox = new TextBox();
             SuspendLayout();
             // 
             // inputTextBox
@@ -59,13 +61,22 @@ namespace ImageFileNamer
             inputLimitBox.Text = "Limit";
             inputLimitBox.KeyPress += InputBox_KeyPress;
             // 
+            // sizeBox
+            // 
+            sizeBox.Location = new Point(12, 62);
+            sizeBox.Name = "sizeBox";
+            sizeBox.Size = new Size(200, 23);
+            sizeBox.TabIndex = 2;
+            sizeBox.Text = "Size (optional)";
+            sizeBox.KeyPress += InputBox_KeyPress;
+            // 
             // generateButton
             // 
             generateButton.FlatStyle = FlatStyle.Flat;
-            generateButton.Location = new Point(12, 63);
+            generateButton.Location = new Point(12, 89);
             generateButton.Name = "generateButton";
-            generateButton.Size = new Size(200, 82);
-            generateButton.TabIndex = 2;
+            generateButton.Size = new Size(200, 54);
+            generateButton.TabIndex = 3;
             generateButton.Text = "Generate";
             generateButton.Click += GenerateButton_Click;
             // 
@@ -77,7 +88,7 @@ namespace ImageFileNamer
             closeCheckBox.Location = new Point(12, 148);
             closeCheckBox.Name = "closeCheckBox";
             closeCheckBox.Size = new Size(128, 19);
-            closeCheckBox.TabIndex = 3;
+            closeCheckBox.TabIndex = 4;
             closeCheckBox.Text = "Close after copying";
             closeCheckBox.UseVisualStyleBackColor = true;
             // 
@@ -89,7 +100,7 @@ namespace ImageFileNamer
             confirmationMsg.Location = new Point(12, 168);
             confirmationMsg.Name = "confirmationMsg";
             confirmationMsg.Size = new Size(187, 19);
-            confirmationMsg.TabIndex = 4;
+            confirmationMsg.TabIndex = 5;
             confirmationMsg.Text = "Don't show confirmation msg.";
             confirmationMsg.UseVisualStyleBackColor = true;
             // 
@@ -101,6 +112,7 @@ namespace ImageFileNamer
             Controls.Add(confirmationMsg);
             Controls.Add(closeCheckBox);
             Controls.Add(inputTextBox);
+            Controls.Add(sizeBox);
             Controls.Add(inputLimitBox);
             Controls.Add(generateButton);
             ForeColor = SystemColors.Control;
@@ -113,9 +125,10 @@ namespace ImageFileNamer
         private void GenerateButton_Click(object sender, EventArgs e)
         {
             string input = inputTextBox.Text.Trim();
-            int limit;
             string filename = Path.GetFileNameWithoutExtension(input);
             string ext = Path.GetExtension(input);
+            int limit;
+            int? size = null;
             int counter = 1;
 
             if (string.IsNullOrEmpty(input))
@@ -136,16 +149,41 @@ namespace ImageFileNamer
                 return;
             }
 
+            if (int.TryParse(sizeBox.Text, out int parsedSize))
+            {
+                if (parsedSize > 1)
+                {
+                    size = parsedSize;
+                }
+                else if (parsedSize < 1)
+                {
+                    MessageBox.Show("Please enter a valid resolution");
+                    return;
+                }
+            }
+            else
+            {
+                size = null;
+            }
+
+
             using (StringWriter stringWriter = new StringWriter())
             {
                 // Console.WriteLine(input); // Prints as redundancy
-                stringWriter.WriteLine(input); // Add orig. filename to clipboard list
+                input = size.HasValue 
+                    ? $"![[{input}|{size}]]" 
+                    : $"![[{input}]]"; // if-state. shorthand / null coalescing operator
+                stringWriter.WriteLine(input); // Write orig. filename to clipboard list
 
                 // Iter. up to the limit
                 while (counter <= limit)
                 {
-                    string newFilename = $"{filename}-{counter}{ext}";
-                    // Console.WriteLine(newFilename); // Redundancy in case clipboard fails
+                    // if-state. shorthand / null coalescing operator
+                    string newFilename = size.HasValue // if size, then, else
+                        ? $"![[{filename}-{counter}{ext}|{size}]]" 
+                        : $"![[{filename}-{counter}{ext}]]";
+
+                    Console.WriteLine(newFilename); // Redundancy in case clipboard fails
                     stringWriter.WriteLine(newFilename); // Adds filename iter. to clipboard
                     counter++; // Incr. counter to reach user req. limit
                 }
